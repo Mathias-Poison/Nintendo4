6167bc3046f5f467f3a61f61a679d21be3436fc7
Guillaume <guillaumevin-gv@hotmail.fr>
2023-02-16 10:16:41 +0100

    addAchat

--- NintendoProject/src/main/java/model/Client.java
+++ NintendoProject/src/main/java/model/Client.java
@@ -1,14 +1,20 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client {
 
 	private String nom;
 	private String prenom;
+	private List<Achat> achats= new ArrayList() ;
+	
+	
 	public Client(String nom, String prenom) {
-		super();
 		this.nom = nom;
 		this.prenom = prenom;
 	}
+	
 	public String getNom() {
 		return nom;
 	}
@@ -21,9 +27,19 @@ public class Client {
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+
+
 	@Override
 	public String toString() {
 		return "Client [nom=" + nom + ", prenom=" + prenom + "]";
+	}
+
+	public List<Achat> getAchats() {
+		return achats;
+	}
+
+	public void setAchats(List<Achat> achats) {
+		this.achats = achats;
 	}
 	
 	
--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,14 +1,21 @@
 package test;
 
+import model.Client;
 import model.Console;
+
 import model.Jeu;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Achat;
 
 public class Test {
-
+	static List<Achat> listAchat1 = new ArrayList(); 
+	static List<Achat> listAchat2 = new ArrayList(); 
+	static List<Achat> listAchat3 = new ArrayList(); 
+	
 	public static void main(String[] args) {
 		
 		Console console1 = new Console ("Gameboy");
@@ -24,8 +31,17 @@ public class Test {
 		Jeu jeu5 = new Jeu ("Metroid",console5);
 		
 		Achat achat1 = new Achat (jeu1,LocalDate.now(),45.0);
+		Achat achat2 = new Achat (jeu2,LocalDate.now(),45.0);
+	
 		
 		System.out.println(achat1);
+		
+		Client c1 = new Client("Ali","Baba");
+		Client c2 = new Client("Baba","Haut-rom");
+		Client c3 = new Client("Baba","Cool");
+		
+		c1.getAchats().add(achat1);
+		c1.getAchats().add(achat2);
 		
 	}
 
